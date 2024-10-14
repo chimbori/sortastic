93eafa4f832f29049fab1c377f890a60d437a930
Chimbori <hello@chimbori.com>
2024-10-13 22:12:15 -0700

    Add `net.go` with network utils

---
+++ conf/net.go
@@ -0,0 +1,14 @@
+package conf
+
+import (
+	"net"
+)
+
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil
+	}
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP
+}
